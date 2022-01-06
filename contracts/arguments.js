9e68826ae42a287bd41ff2572b46f810a1390670
0xTuytuy <rtuyaerts@hotmail.com>
2022-01-06 19:06:00 +0100

    adding some arguments

---
+++ contracts/arguments.js
@@ -0,0 +1,3 @@
+module.exports = [
+  process.env.NEW_ADMIN,
+];
--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,16 +1,26 @@
 import { ethers } from "hardhat";
 import {AlluoToken__factory} from "../typechain"
 
+const { exec } = require('child_process');
+const hre = require("hardhat");
+
 async function deploy() {
 
    // We get the contract to deploy
   const factory = await ethers.getContractFactory('AlluoToken') as AlluoToken__factory
-  
-  console.log("deploying on live chain")
+  console.log("Deploying on live chain")
   const alluoToken = await factory.deploy("0xfb7A51c6f6A5116Ac748C1aDF4D4682c3D50889E");
   console.log("AlluoToken deploying to:", alluoToken.address);
   await alluoToken.deployed();
-  console.log("! deployed !");
+  console.log("! Deployed !");
+
+  // For verification of the contract run: npx hardhat verify --network kovan --constructor-args contracts/arguments.js 0xb625dA6b38FD7c62747c777121140cA4b3D7fD05
+  await hre.run("verify:verify", {
+    address: alluoToken.address,
+    constructorArguments: [
+      process.env.NEW_ADMIN
+    ],
+  });
 }
 
 deploy()
